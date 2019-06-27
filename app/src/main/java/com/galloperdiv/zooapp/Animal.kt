e6a1f01086e7bc05bad31dcd8a6f4f764b20e9d2
alie el dine <alieeldine@gmail.com>
2019-06-27 07:03:44 +0200

    inial commit

---
+++ app/src/main/java/com/galloperdiv/zooapp/Animal.kt
@@ -0,0 +1,12 @@
+package com.galloperdiv.zooapp
+
+class Animal {
+    var name:String?=null
+    var desc:String?=null
+    var image:Int?=null
+    constructor(name:String, desc:String, image:Int) {
+        this.name = name
+        this.desc = desc
+        this.image = image
+    }
+}
